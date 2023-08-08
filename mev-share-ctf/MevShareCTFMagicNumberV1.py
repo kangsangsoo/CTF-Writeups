61b95ea14a9a6516028138bde2dbc58a60db73f0
kangsangsoo <rkdtkdtn0706@korea.ac.kr>
2023-08-08 17:23:18 +0900

    cleanup

--- mev-share-ctf/MevShareCTFMagicNumberV1.py
+++ mev-share-ctf/MevShareCTFMagicNumberV1.py
@@ -14,119 +14,12 @@ relay_url = "https://relay-goerli.flashbots.net/"
 
 w3 = Web3(Web3.HTTPProvider(rpc_url))
 
-
-# install_solc("0.8.19")
-
-# sources = open("Checker.sol", "r").read()
-# compiled_sol = compile_source(sources, output_values=['abi', 'bin'])
-
-
-# abi, bin = compiled_sol["<stdin>:Checker"]['abi'], compiled_sol["<stdin>:Checker"]['bin']
-
-# print(abi)
-# print(bin)
-
-# my_account = w3.eth.account.from_key(private_key)
-
-# checker = w3.eth.contract(abi=abi, bytecode=bin)
-# transaction = checker.constructor().build_transaction({
-#     "chainId": w3.eth.chain_id,
-#     "gasPrice": w3.eth.gas_price,
-#     "from": my_account.address,
-#     "nonce": w3.eth.get_transaction_count(my_account.address),
-# })
-
-
-# tx_hash = w3.eth.send_raw_transaction(my_account.sign_transaction(transaction).rawTransaction)
-# rec = w3.eth.wait_for_transaction_receipt(tx_hash)
-# print(rec)
-
-# checker_addr = rec.contractAddress
-# print(checker_addr) 
-
-checker_addr = "0xbE0d1B1990Cec561c6d9e178C51148E268EA97aD"
-# checker = w3.eth.contract(address=checker_addr, abi=abi)
-
-
-# def event_stream(url):
-#     while True:
-#         event = []
-#         try:
-#             with requests.get(url, stream=True, timeout=4) as response:
-            
-#                 for line in response.iter_lines():
-#                     if line:  # filter out keep-alive new lines
-#                         event.append(line)
-#         except:
-#                 yield event
-
 def event_stream(url):
     with requests.get(url, stream=True) as response:
         for line in response.iter_lines():
             if line:  # filter out keep-alive new lines
                 yield line
 
-
-def send_bundle(hash, block_number, magic_number, nonce, to):
-    data = {
-        "jsonrpc": "2.0",
-        "id": "1",
-        "method": "mev_sendBundle",
-        "params": [
-            {
-                "version": "v0.1",
-                "inclusion": {
-                    "block": hex(block_number),
-                    "maxBlock": hex(block_number+20),
-                },
-                "body": [
-                    {'hash': hash},
-                ],
-            }
-        ],
-    }
-    data['params'][0]['body'].append({
-        "tx": Account.sign_transaction(
-            {
-                "gas": 10 ** 12,
-                "maxFeePerGas": 2 * 10 ** 13,
-                "maxPriorityFeePerGas": 2 * 10 ** 13,
-                "data": "0xae169a50" + hex(magic_number)[2:].rjust(64, '0'), 
-                "nonce": nonce,
-                "to": to, 
-                "value": "0x0", 
-                "chainId": 5,
-            }, 
-            private_key
-        ).rawTransaction.hex(),
-        "canRevert": False,
-    })
-    data['params'][0]['body'].append({
-        "tx": Account.sign_transaction(
-            {
-                "gas": 10 ** 12,
-                "maxFeePerGas": 2 * 10 ** 13,
-                "maxPriorityFeePerGas": 2 * 10 ** 13,
-                "data": "0xcf5303cf", 
-                "nonce": nonce + 1,
-                "to": checker_addr, 
-                "value": "0x0", 
-                "chainId": 5,
-            }, 
-            private_key
-        ).rawTransaction.hex(),
-        "canRevert": False,
-    })
-    # print(data)
-    message = encode_defunct(text=Web3.keccak(text=json.dumps(data)).hex())
-    headers = {
-        "Content-Type": "application/json",
-        "X-Flashbots-Signature": Account.from_key(private_key).address + ":" +  Account.sign_message(message, private_key).signature.hex(),
-    }
-    print(requests.post(url=relay_url, data=json.dumps(data), headers=headers).content)
-
-
-ADDR = Web3.to_checksum_address(0xE8B7475e2790409715AF793F799f3Cc80De6f071)
 
 for events in event_stream(sse_url):
     try:
